79c71d33c79b0b53547a5bb25095d8137637f845
Jeffreyo3 <Jeffrey.Orndorff@gmail.com>
2021-02-20 11:09:31 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+const ffmpeg = createFFmpeg({ log: true });
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
