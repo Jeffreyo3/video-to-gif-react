ebadb318917ceebb4028d398c6828a999a2c7778
Jeffreyo3 <Jeffrey.Orndorff@gmail.com>
2021-02-20 11:20:23 -0800

    Implement ability to add video and view preview

--- src/App.js
+++ src/App.js
@@ -5,7 +5,27 @@ import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 const ffmpeg = createFFmpeg({ log: true });
 
 function App() {
-  return <div className="App"></div>;
+  const [ready, setReady] = useState(false);
+  const [video, setVideo] = useState();
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  };
+
+  useEffect(() => {
+    load();
+  }, []);
+  return ready ? (
+    <div className="App">
+      {video && (
+        <video controls width="250" src={URL.createObjectURL(video)}></video>
+      )}
+      <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
+    </div>
+  ) : (
+    <p>Loading...</p>
+  );
 }
 
 export default App;
